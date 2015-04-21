c85d4a43ca1e0ee984f1702f18d4f488ebadc233
Dony Thomas <dony.thomas7@gmail.com>
2015-04-21 16:29:16 -0400

    4th commit

--- AngularLearning/src/main/webapp/resources/custom/js/controllers.js
+++ AngularLearning/src/main/webapp/resources/custom/js/controllers.js
@@ -7,13 +7,45 @@ angular.module('myApp.controllers').controller('FinanceController',
 			$scope.result = function() {
 				return $scope.salary * $scope.percentage * 0.01;
 			};
-			console.log($scope.__proto__ === $rootScope);
-			console.log($rootScope.__proto__.constructor.name);
-			console.log($rootScope.__proto__.__proto__ === Object.prototype);
+			$scope.$watch('percentage', function(newValue, oldValue) {
+				console.log('watch: ' + newValue + ' -- ' + oldValue);
+				if (newValue == 100) {
+					window.alert('100%');
+					$scope.salary = 100;
+				}
+			});
+			$scope.$watch(function() {
+				// do something here
+				console.log('called in a digest cycle');
+				return;
+			});
 		});
 
-angular.module('myApp.controllers').controller('DateTimeController',
-		function($scope) {
-			$scope.now = new Date();
-			$scope.greeting = "Hello";
-		});
+var Task = function(description) {
+	this.done = 'N';
+	this.description = description;
+};
+
+angular.module('myApp.controllers').controller('TODOController',
+		[ '$scope', function($scope) {
+			$scope.newTaskDescription = '';
+			$scope.taskList = [];
+			$scope.addTask = function() {
+				if ($scope.newTaskDescription.length > 0) {
+					$scope.taskList.push(new Task($scope.newTaskDescription));
+					$scope.newTaskDescription = '';
+				}
+			};
+			$scope.deleteTask = function(task) {
+				var removeIndex = 0;
+				for (var i = 0; i < $scope.taskList.length; i++) {
+					if ($scope.taskList[i].description === task.description) {
+						removeIndex = i;
+					}
+				}
+				if (removeIndex > -1) {
+					$scope.taskList.splice(removeIndex, 1);
+				}
+			};
+
+		} ]);
