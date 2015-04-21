a994f030b3555e71ecfbd0b8b62d060d742e0ae6
Dony Thomas <dony.thomas7@gmail.com>
2015-04-21 00:50:47 -0400

    3rd commit

--- AngularLearning/src/main/webapp/resources/custom/js/controllers.js
+++ AngularLearning/src/main/webapp/resources/custom/js/controllers.js
@@ -1,12 +1,15 @@
 angular.module('myApp.controllers', []);
 
 angular.module('myApp.controllers').controller('FinanceController',
-		function($scope) {
+		function($rootScope, $scope) {
 			$scope.salary = 0;
 			$scope.percentage = 0;
 			$scope.result = function() {
 				return $scope.salary * $scope.percentage * 0.01;
 			};
+			console.log($scope.__proto__ === $rootScope);
+			console.log($rootScope.__proto__.constructor.name);
+			console.log($rootScope.__proto__.__proto__ === Object.prototype);
 		});
 
 angular.module('myApp.controllers').controller('DateTimeController',
