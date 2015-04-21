0f8da7816cb32a90ada01d7d19d3c89ee898c843
Dony Thomas <dony.thomas7@gmail.com>
2015-04-20 22:19:23 -0400

    2nd commit

--- AngularLearning/src/main/webapp/resources/custom/js/controllers.js
+++ AngularLearning/src/main/webapp/resources/custom/js/controllers.js
@@ -1,9 +1,16 @@
-angular.module('myApp.controllers', []).
-controller('FinanceController',
+angular.module('myApp.controllers', []);
+
+angular.module('myApp.controllers').controller('FinanceController',
 		function($scope) {
 			$scope.salary = 0;
 			$scope.percentage = 0;
 			$scope.result = function() {
 				return $scope.salary * $scope.percentage * 0.01;
 			};
+		});
+
+angular.module('myApp.controllers').controller('DateTimeController',
+		function($scope) {
+			$scope.now = new Date();
+			$scope.greeting = "Hello";
 		});
